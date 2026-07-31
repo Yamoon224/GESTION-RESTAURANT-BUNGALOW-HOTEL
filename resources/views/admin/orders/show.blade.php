b61d19a9183ba377da67268c1d0cf74844ad3830
Yamoussa KEITA <160267741+Yamoon224@users.noreply.github.com>
2026-07-31 20:48:06 +0000

    Change fallback text for missing product name

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -34,7 +34,7 @@
                                         <tbody>
                                             @foreach ($order->order_details as $item)
                                                 <tr>
-                                                    <td>{{ optional($item->product)->name ?? 'Produit supprimé' }}</td>
+                                                    <td>{{ optional($item->product)->name ?? '---' }}</td>
                                                     <td>{{ $item->qty }}</td>
                                                     <td>{{ moneyFormat($item->price) }}</td>
                                                     <td>{{ moneyFormat($item->amount) }}</td>
